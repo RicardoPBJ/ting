b3f301aede7870a751036a4cc942122f951557e1
Ricardo <rbrumjunio@gmail.com>
2023-06-28 15:27:37 -0300

    Implemente uma fila para armazenar os arquivos que serão lidos.

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,28 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return self.size()
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        return self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if not self.is_empty():
+            return self._data.pop(0)
+        else:
+            raise IndexError("Índice Inválido ou Inexistente")
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index >= 0 and index < self.size():
+            return self._data[index]
+        else:
+            raise IndexError('Índice Inválido ou Inexistente')
+
+    def is_empty(self):
+        return self._data == []
+
+    def size(self):
+        return len(self._data)
