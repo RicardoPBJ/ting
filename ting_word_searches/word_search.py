6763207ea45c6854d78b0c58561141fa2046677e
Ricardo <rbrumjunio@gmail.com>
2023-06-29 17:42:58 -0300

    Implemente uma função exists_word, dentro do módulo word_search, que verifique a existência de uma palavra em todos os arquivos processados.

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+
+def exists_word(word: str, instance):
+    result = list()
+    for i in range(len(instance)):
+        search = instance.search(i)
+        lines_searched = [
+            {"linha": index + 1}
+            for index, line in enumerate(search['linhas_do_arquivo'])
+            if word.lower() in line.lower()
+        ]
+
+        if lines_searched:
+            obj = {
+                "palavra": word,
+                "arquivo": search["nome_do_arquivo"],
+                "ocorrencias": lines_searched
+            }
+            result.append(obj)
+
+    return result
 
 
 def search_by_word(word, instance):
